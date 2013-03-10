3c021d26cfc5c148c6643e1cbb2ec665ea05c09f
Chun <me@lichun.cc>
2013-03-10 19:01:23 -0400

    vim changes

---
+++ bundle/run.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+git clone https://github.com/scrooloose/nerdtree.git nerdtree
+git clone https://github.com/Lokaltog/vim-easymotion.git easymotion
+git clone https://github.com/davidhalter/jedi-vim.git jedi-vim
+git clone https://github.com/scrooloose/nerdcommenter.git nerdcommenter
+git clone https://github.com/fholgado/minibufexpl.vim.git minibufexpl
+git clone https://github.com/scrooloose/syntastic.git syntastic
+git clone https://github.com/godlygeek/tabular.git tabular
+git clone https://github.com/pangloss/vim-javascript.git vim-javascript
