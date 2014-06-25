2d6c76d5e0a1c06f02b6389eb58e52de3006ce19
Chun Li <me@lichun.cc>
2014-06-25 16:59:14 -0400

    add update script

---
+++ bundle/update.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+cd nerdtree
+git pull
+cd ..
+cd easymotion
+git pull
+cd ..
+#git clone https://github.com/davidhalter/jedi-vim.git jedi-vim
+cd nerdcommenter
+git pull
+cd ..
+cd minibufexpl
+git pull
+cd ..
+cd syntastic
+git pull
+cd ..
+cd tabular
+git pull
+cd ..
+cd vim-javascript
+git pull
+cd ..
+cd vim-colors-solarized
+git pull
+cd ..
