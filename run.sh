3d7d2a79f21fd379cae6ee71542db04ef6207487
chun <l@xkool.org>
2018-02-18 05:58:05 -0500

    added vim plugin; fix the powerline-status name

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ mkdir bundle
 mkdir undo
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 brew install CMake
-pip3 install power-status
+pip3 install powerline-status
 cp .vimrc ~/
 vim README.md
 ./bundle/YouCompleteMe/install.py --clang-completer
